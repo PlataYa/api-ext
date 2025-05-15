bb9ede58a7f03a04708e131aba15da16be959598
mardejesus <mardejesus001@gmail.com>
2025-05-15 08:21:56 -0300

    first commit, kotlin + spring boot setup.

---
+++ src/test/kotlin/com/plataya/external_api/PlataYaExternalApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.plataya.external_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlataYaExternalApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
