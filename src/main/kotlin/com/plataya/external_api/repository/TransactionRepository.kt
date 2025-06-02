d8dd4059506f9f6fdeb76009e38a39fa40a9fca4
Elias85974 <eballesteros@mail.austral.edu.ar>
2025-06-02 16:54:59 -0300

    Refactor TransactionRepository to use updated model package for Transaction entity

--- src/main/kotlin/com/plataya/external_api/repository/TransactionRepository.kt
+++ src/main/kotlin/com/plataya/external_api/repository/TransactionRepository.kt
@@ -1,10 +1,9 @@
 package com.plataya.external_api.repository
-import com.plataya.external_api.entity.Transaction
+import com.plataya.external_api.model.entity.Transaction
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
 interface TransactionRepository : JpaRepository<Transaction, UUID> {
-
 }
