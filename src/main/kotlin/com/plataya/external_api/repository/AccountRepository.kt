7a9260270a17defad55e22b8295931bad9e403be
Elias85974 <eballesteros@mail.austral.edu.ar>
2025-06-02 16:55:19 -0300

    Refactor AccountRepository to use updated model package for Account entity and change ID to be the cvu

--- src/main/kotlin/com/plataya/external_api/repository/AccountRepository.kt
+++ src/main/kotlin/com/plataya/external_api/repository/AccountRepository.kt
@@ -1,11 +1,8 @@
 package com.plataya.external_api.repository
-import com.plataya.external_api.entity.Account
+import com.plataya.external_api.model.entity.Account
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.util.UUID
 
 @Repository
-interface AccountRepository : JpaRepository<Account, UUID> {
-    fun existsByCvu(cvu: String): Boolean
-    fun findByCvu(cvu: String): Account?
+interface AccountRepository : JpaRepository<Account, String> {
 }
